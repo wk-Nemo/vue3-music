56104fb8ebda754e881410c7746aa392f436a89e
wk-Nemo <2181544173@qq.com>
2021-11-28 17:02:12 +0800

    singer: index-list finish

--- vue3-music/src/views/singer.vue
+++ vue3-music/src/views/singer.vue
@@ -1,9 +1,42 @@
 <template>
-  this is singer
+  <div class="singer" v-loading="loading">
+    <IndexList
+      :data="singers"
+    ></IndexList>
+  </div>
 </template>
 
 <script>
+import { getSingerList } from '@/service/singer'
+import IndexList from '@/components/index-list/index-list'
+
 export default {
-  name: 'singer'
+  name: 'singer',
+  components: {
+    IndexList
+  },
+  data() {
+    return {
+      singers: []
+    }
+  },
+  computed: {
+    loading() {
+      return !this.singers.length
+    }
+  },
+  async created() {
+    const result = await getSingerList()
+    this.singers = result.singers
+  }
 }
 </script>
+
+<style lang="scss" scoped>
+.singer {
+  position: fixed;
+  width: 100%;
+  top: 88px;
+  bottom: 0;
+}
+</style>
